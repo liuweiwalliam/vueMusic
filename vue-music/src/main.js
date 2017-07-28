b4ef1d5ea0d47040bc8e4056160070e787c4aa13
liuweiwalliam <liuweiwalliam@163.com>
2017-07-28 18:33:36 +0800

    tets

---
+++ vue-music/src/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by walliam on 2017/7/28.
+ */
+import Vue from 'vue'
+import App from './App'
+import 'common/stylus/index.tpl'
+var VueApp = new Vue({
+  el: '#app',
+  render: h => h(App)
+
+})
+
---
+++ vue-music/src/router/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by walliam on 2017/7/28.
+ */
