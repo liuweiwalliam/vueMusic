d7334f2753071ccae0ad104db698ee174c722b0b
walliam <liuweiwalliam@163.com>
2017-09-22 23:35:40 +0800

    upd

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -38,10 +38,11 @@
         this.children = this.$refs.sliderGroup.children;
         let width = 0;
         let sliderWidth = this.$refs.slider.clientWidth;
-        for (let i = 0; i < this.length; i++) {
-          let child = this.children;
+        console.log(this.children);
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i];
           addClass(child, 'slider-item');
-          child.style.width = sliderWidth;
+          child.style.width = sliderWidth+'px';
           width += sliderWidth;
         }
         ;
