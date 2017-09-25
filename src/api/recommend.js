8c1910088e34d782874a0b97f3995eee25011aaf
liuweiwalliam <liuweiwalliam@163.com>
2017-09-25 15:47:50 +0800

    upd

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -12,5 +12,21 @@ export function getRecommend() {
 }
 
 export function getDiscList() {
-  const url = '';
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg';
+  const _ajData = {
+    platform: 'yqq',
+    hostUin: 0,
+    loginUin:0,
+    inCharset:'utf8',
+    outCharset:'utf-8',
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    needNewCode: 0,
+    categoryId: 10000000,
+    rnd: Math.random()
+  };
+  const data = Object.assign({}, commonParams, _ajData);
+  return jsonp(url, data, options)
 }
+
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,51 +1,52 @@
 <template>
   <div class="recommend">
     <!--<scroll ref="scroll" class="recommend-content" >-->
-      <div>
-        <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
-          <slider>
-            <div v-for="item in recommends">
-              <a :href="item.linkUrl">
-                <img class="needsclick"  :src="item.picUrl">
-                <!--@load="loadImage"-->
-              </a>
-            </div>
-          </slider>
-        </div>
-        <!--<div class="recommend-list">-->
-          <!--<h1 class="list-title">热门歌单推荐</h1>-->
-          <!--<ul>-->
-            <!--<li class="item">-->
-              <!--<div class="icon">-->
-                <!--<img width="60" height="60">-->
-              <!--</div>-->
-              <!--<div class="text">-->
-                <!--<h2 class="name"></h2>-->
-                <!--<p class="desc"></p>-->
-              <!--</div>-->
-            <!--</li>-->
-          <!--</ul>-->
-        <!--</div>-->
+    <div>
+      <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img class="needsclick" :src="item.picUrl">
+              <!--@load="loadImage"-->
+            </a>
+          </div>
+        </slider>
       </div>
-      <div class="loading-container">
-        <!--<loading></loading>-->
-      </div>
+      <!--<div class="recommend-list">-->
+      <!--<h1 class="list-title">热门歌单推荐</h1>-->
+      <!--<ul>-->
+      <!--<li class="item">-->
+      <!--<div class="icon">-->
+      <!--<img width="60" height="60">-->
+      <!--</div>-->
+      <!--<div class="text">-->
+      <!--<h2 class="name"></h2>-->
+      <!--<p class="desc"></p>-->
+      <!--</div>-->
+      <!--</li>-->
+      <!--</ul>-->
+      <!--</div>-->
+    </div>
+    <div class="loading-container">
+      <!--<loading></loading>-->
+    </div>
     <!--</scroll>-->
     <router-view></router-view>
   </div>
 </template>
 <script type="text/ecmascript-6">
-  import { getRecommend } from '../../api/recommend'
-  import { ERR_OK } from '../../api/config'
+  import {getRecommend, getDiscList} from '../../api/recommend'
+  import {ERR_OK} from '../../api/config'
   import Slider from '../../base/slider/slider'
   export default{
     created (){
-      this._getRecommend()
+      this._getRecommend();
+      this._getDiscList()
     },
     data(){
-        return{
-          recommends:[]
-        }
+      return {
+        recommends: []
+      }
     },
     methods: {
       _getRecommend(){
@@ -58,9 +59,16 @@
 
           }
         })
+      },
+      _getDiscList(){
+        getDiscList().then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res);
+          }
+        })
       }
     },
-    components:{Slider}
+    components: {Slider}
   }
 </script>
 
@@ -72,44 +80,44 @@
     top: 88px
     bottom: 0
     /*.recommend-content*/
-      /*height: 100%*/
-      /*overflow: hidden*/
-      .slider-wrapper
-        position: relative
-        width: 100%
-        overflow: hidden
-      .recommend-list
-        .list-title
-          height: 65px
-          line-height: 65px
-          text-align: center
-          font-size: $font-size-medium
-          color: $color-theme
-        .item
+    /*height: 100%*/
+    /*overflow: hidden*/
+    .slider-wrapper
+      position: relative
+      width: 100%
+      overflow: hidden
+    .recommend-list
+      .list-title
+        height: 65px
+        line-height: 65px
+        text-align: center
+        font-size: $font-size-medium
+        color: $color-theme
+      .item
+        display: flex
+        box-sizing: border-box
+        align-items: center
+        padding: 0 20px 20px 20px
+        .icon
+          flex: 0 0 60px
+          width: 60px
+          padding-right: 20px
+        .text
           display: flex
-          box-sizing: border-box
-          align-items: center
-          padding: 0 20px 20px 20px
-          .icon
-            flex: 0 0 60px
-            width: 60px
-            padding-right: 20px
-          .text
-            display: flex
-            flex-direction: column
-            justify-content: center
-            flex: 1
-            line-height: 20px
-            overflow: hidden
-            font-size: $font-size-medium
-            .name
-              margin-bottom: 10px
-              color: $color-text
-            .desc
-              color: $color-text-d
-      .loading-container
-        position: absolute
-        width: 100%
-        top: 50%
-        transform: translateY(-50%)
+          flex-direction: column
+          justify-content: center
+          flex: 1
+          line-height: 20px
+          overflow: hidden
+          font-size: $font-size-medium
+          .name
+            margin-bottom: 10px
+            color: $color-text
+          .desc
+            color: $color-text-d
+    .loading-container
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
 </style>
